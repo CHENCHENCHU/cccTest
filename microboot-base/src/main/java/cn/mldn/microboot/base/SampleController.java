8c8663264b6e75b46a23252767fc1a08ef825c72
m1885 <m1885@ccc>
2018-03-28 11:01:44 +0800

    test2

--- microboot-base/src/main/java/cn/mldn/microboot/base/SampleController.java
+++ microboot-base/src/main/java/cn/mldn/microboot/base/SampleController.java
@@ -12,7 +12,7 @@ public class SampleController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String home() {
-		return "Hello World!";
+		return "Hello World!!!";
 	}
 
 	public static void main(String[] args) throws Exception {
